357d1f3fc221f0810929fc21bbff8b7039358ebe
Nisc3d <nisc3c@protonmail.com>
2019-03-20 14:18:27 +0100

    Initial commit

---
+++ src/Augabe1/Algorithmen.java
@@ -0,0 +1,17 @@
+package Augabe1;
+
+public class Algorithmen {
+
+    public double iterativfib(int zahl) {
+        int i = 0;
+        double[] reihe = new double[100000];
+        double[] ergebnis = new double[100000];
+        while (reihe[i] <= zahl){
+            ergebnis[i] = 1;
+
+        }
+
+
+        return zahl;
+    }
+}
---
+++ src/Augabe1/Main.java
@@ -0,0 +1,24 @@
+package Augabe1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double zahl = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eingabe Zahl: ");
+        zahl = sc.nextDouble();
+        if (zahl >= 0){
+
+
+
+        }
+        else{
+            System.out.println("Bitte natürliche Zahl eingeben.");
+        }
+
+
+
+    }
+}
