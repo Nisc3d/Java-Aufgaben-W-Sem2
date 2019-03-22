987e15b71677996f0fe354b8acd2227f9f35a26e
Nisc3d <nisc3d@protonmail.com>
2019-03-22 19:14:57 +0100

    Finnish Aufgabe2

--- src/Aufgabe2/sort.java
+++ src/Aufgabe2/sort.java
@@ -2,24 +2,31 @@ package Aufgabe2;
 
 public class sort {
 
-    public void printArray (int[] array) {
-        for (int i = 0; i < array.length; i++)
-            System.out.print (array[i] + " ");
-        System.out.println ();
+    public int[] initArray(int num) {
+        int[] result = new int[num];
+        for (int i = 0; i < num; i++)
+            result[i] = (int) (Math.random() * 100.0);
+        return result;
     }
 
-    public void insertionSort(int[] array){
+    public void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+
+    public void insertionSort(int[] array) {
         int m, j;
-        for (int i = 1; i < array.length; i += 1 ){
+        for (int i = 1; i < array.length; i += 1) {
             m = array[i];
             j = i;
 
-            while (j > 0){
-                if ( array[j-1] >= m){
-                    array[j] = array[j-1];
+            while (j > 0) {
+                if (array[j - 1] >= m) {
+                    array[j] = array[j - 1];
                     j -= 1;
                 }
-                else{
+                else {
                     break;
                 }
             }
@@ -27,23 +34,40 @@ public class sort {
         }
     }
 
-    public void selectionSort(int[] array){
-        int p = array.length, g = 0;
-        while (p > 0){
-            g = groesterWert(array);
-            
-
+    public void selectionSort(int[] array) {
+        int p = array.length - 1, g = 0, hilfsvar = 0;
+        while (p > 0) {
+            g = groesterWertIndex(array, 0, p);
+            switchPosIntArr(array, p, g);
+            p -= 1;
         }
     }
 
-    public int groesterWert(int[] array){
-        int i = 1, wert = array[0];
-        while (i < array.length){
-            if (array[i] > wert){
+    public int groesterWertIndex(int[] array, int lowlimit, int highlimit) {
+        int i = lowlimit + 1, wert = array[lowlimit], wertindex = lowlimit;
+        while (i <= highlimit) {
+            if (array[i] > wert) {
                 wert = array[i];
+                wertindex = i;
             }
             i += 1;
         }
-        return wert;
+        return wertindex;
     }
+
+    public void switchPosIntArr(int[] array, int pos1, int pos2){
+        int hilfsvar = 0;
+        hilfsvar = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos2] = hilfsvar;
+    }
+
+    public void switchPosStringArr(String[] array, int pos1, int pos2){
+        String hilfsvar = "";
+        hilfsvar = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos2] = hilfsvar;
+    }
+
+
 }
